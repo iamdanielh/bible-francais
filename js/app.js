cda6980af9ecd9ab8ac72dd3d1cff1de97e8bf3c
dangel <dangel@localhost>
2026-09-17 20:18:15 -0400

    UX: selector de idioma pegado al título, muy a la izquierda
    Saco el idioma de los controles (derecha) y lo integro junto a 'BiblioLingo',
    con estilo pill compacto. Bump v41/biblefr-v19.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { Dictionary, friendlyTense, friendlyForm, esInfinitive, esConjugado, esCompuesto } from "./dict.js?v=40";
+import { Dictionary, friendlyTense, friendlyForm, esInfinitive, esConjugado, esCompuesto } from "./dict.js?v=41";
 
 // ---- language packs --------------------------------------------------------
 // Every language is a self-contained pack: which data files to fetch, how to
